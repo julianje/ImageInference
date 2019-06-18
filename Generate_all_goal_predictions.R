aa1beed888e497e1fb80a547203d334d3a39857b
julianje <julian.jara-ettinger@yale.edu>
2019-06-18 14:06:02 -0400

    Better naming scheme

--- Generate_all_goal_predictions.R
+++ Generate_all_goal_predictions.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-# Loads all goal inferences files and combines them into a single one.
+# Loads all goal inferences files and combines them into a single
 
 # Load everything ------------------------------------------
 directory = "~/Documents/Projects/Models/ImageInference/ImageInference/"
@@ -10,8 +10,9 @@ setwd(directory)
 GoalPredictions <- data.frame(Files = list.files("ModelPredictions/")) %>%
   mutate(States=str_detect(Files,"Goal")) %>% tbl_df %>% filter(States)
 
-goalposterior <- do.call(bind_rows,lapply(GoalPredictions$Files,function(x){mutate(read_csv(paste("ModelPredictions/",x,sep=""),
-                                                                                            col_types=cols()),Trial=gsub("_Goal_Posterior.csv","",x))})) %>%
+goalposterior <- do.call(bind_rows,lapply(GoalPredictions$Files,function(x){
+  mutate(read_csv(paste("ModelPredictions/",x,sep=""),
+                  col_types=cols()),Trial=gsub("_Goal_Posterior.csv","",x))})) %>%
   mutate(GoalLetter=factor(Goal))
 
 levels(goalposterior$GoalLetter) = c("A", "C", "B")
