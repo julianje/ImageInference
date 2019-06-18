952af8c27d32be7deac143626771aa7c4f94b092
julianje <julian.jara-ettinger@yale.edu>
2019-06-18 13:18:44 -0400

    File to combine all goal inferences

---
+++ GenerateAllGoalInferences.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+# Load everything ------------------------------------------
+directory = "~/Documents/Projects/Models/ImageInference/ImageInference/"
+
+setwd(directory)
+
+GoalPredictions <- data.frame(Files = list.files("ModelPredictions/")) %>%
+  mutate(States=str_detect(Files,"Goal")) %>% tbl_df %>% filter(States)
+
+goalposterior <- do.call(bind_rows,lapply(GoalPredictions$Files,function(x){mutate(read_csv(paste("ModelPredictions/",x,sep=""),
+                                                                                            col_types=cols()),Trial=gsub("_Goal_Posterior.csv","",x))})) %>%
+  mutate(GoalLetter=factor(Goal))
+
+levels(goalposterior$GoalLetter) = c("A", "C", "B")
+
+write.csv(goalposterior, "Goal_Inference.csv", quote=F, row.names = F)
