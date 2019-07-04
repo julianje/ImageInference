71b239a0917196ca85bc6170c27f1d682b12090e
Michael Lopez-Brau <michael.lopez.brau@gmail.com>
2019-07-04 01:45:24 -0400

    Added function to toggle choice tau usage.

--- utils/toggle_choice_softmax.sh
+++ utils/toggle_choice_softmax.sh
@@ -4,12 +4,12 @@ if [ $1 = "true" ]
 then
 	for file in $(find ../maps -type f -name "*.ini")
 	do
-		sed -i 's/DiagonalTravel:\ False/DiagonalTravel:\ True/' $file
+		sed -i 's/SoftmaxChoice\ =\ False/SoftmaxChoice\ =\ True/' $file
 	done
 elif [ $1 = "false" ]
 then
 	for file in $(find ../maps -type f -name "*.ini")
 	do
-		sed -i 's/DiagonalTravel:\ True/DiagonalTravel:\ False/' $file
+		sed -i 's/SoftmaxChoice\ =\ False/SoftmaxChoice\ =\ False/' $file
 	done
 fi
