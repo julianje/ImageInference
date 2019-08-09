d1dab92acf5372dc3800dad3d1761e8997593750
Michael Lopez-Brau <michael.lopez.brau@gmail.com>
2019-08-09 14:33:30 -0400

    Updated path info in utils scripts.

--- utils/toggle_action_softmax.sh
+++ utils/toggle_action_softmax.sh
@@ -4,12 +4,12 @@ if [ $1 = "true" ]
 then
 	for file in $(find ../stimuli/experiment_1 -maxdepth 1 -type f -name "*.ini")
 	do
-		sed -i 's/DiagonalTravel:\ False/DiagonalTravel:\ True/' $file
+		sed -i 's/SoftmaxAction\ =\ False/SoftmaxAction\ =\ True/' $file
 	done
 elif [ $1 = "false" ]
 then
 	for file in $(find ../stimuli/experiment_1 -maxdepth 1 -type f -name "*.ini")
 	do
-		sed -i 's/DiagonalTravel:\ True/DiagonalTravel:\ False/' $file
+		sed -i 's/SoftmaxAction\ =\ False/SoftmaxAction\ =\ False/' $file
 	done
 fi
--- utils/update_action_tau.sh
+++ utils/update_action_tau.sh
@@ -2,6 +2,6 @@
 
 TAU=$1
 
-for file in $(find ../maps -type f -name "*.ini"); do
+for file in $(find ../stimuli/experiment_1 -maxdepth 1 -type f -name "*.ini"); do
 	sed -Ei "s/actionTau\ =\ [0-9]+.[0-9]+/actionTau\ =\ $TAU/" $file
 done
